dd9a3742194f75740a30a5ae099ef0672a05abc0
Lauren Pehlivanian <lpehlivanian00@stuy.edu>
2019-04-01 14:53:24 -0400

    fixed get nth digit, passes tests

--- Radix.java
+++ Radix.java
@@ -14,7 +14,7 @@ public class Radix {
       //loop through data.
       for (int idx=0;idx<data.length;idx++) {
         //get specific digit of current int.
-        int digit = Integer.parseInt((""+Math.abs(data[idx])).charAt(i));
+        int digit = Integer.parseInt( ""+ (""+Math.abs(data[idx])).charAt(i));
       }
 
     }
@@ -35,23 +35,29 @@ public class Radix {
   private static int getNthDigit(int num, int n) {
     String s = ""+Math.abs(num);
     //if nth digit doesn't exist (it's 0), add zeroes to s.
-    if (s.length < n) {
-      int zeroes = (n-s.length)+1; // represents zeroes to add.
+
+    if (s.length() < n) return 0;
+
+    int zeroes = (n-s.length())+1; // represents zeroes to add.
+
       //loop to add zeroes to start of new string number
       String s2 = "";
       for (int i=0;i<zeroes;i++) {
         s2 += "0";
       }
       s2 += s;
-    }
-    int idx = s2.length()-n - 1;
-    return Integer.parseInt(s2.charAt(idx));
+
+    int idx = s2.length() - n - 1;
+    return Integer.parseInt(""+s2.charAt(idx));
   }
 
   public static void main(String[] args) {
     int[] data = {1,5,88,99,-1000,599999,-6000000};
     System.out.println(maxDigits(data));
-    System.out.println(getNthDigit(423,0));
+    System.out.println(getNthDigit(423,1));
+    System.out.println(getNthDigit(4233,3));
+    System.out.println(getNthDigit(423,2));
+    System.out.println(getNthDigit(444423,4));
   }
 
 }
