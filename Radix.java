f56d83a15ba35b2321eb70a027df79f3cd4ff298
Lauren Pehlivanian <lpehlivanian00@stuy.edu>
2019-04-02 22:21:21 -0400

    added code to handle negatives, testing

--- Radix.java
+++ Radix.java
@@ -3,8 +3,6 @@ public class Radix {
   public static void radixsort(int[] data){
     int passes = maxDigits(data);
 
-    for (int pass=0;pass<passes;pass++) {
-
       //create array of linkedlists.
       MyLinkedList[] digits = new MyLinkedList[10];
       //initialize eachbucket.
@@ -17,12 +15,17 @@ public class Radix {
         int element = data[idx];
         //get specific digit of current int.
         int digit = getNthDigit(element,0);
-        //add to end of bucket.
-        digits[digit].add(element);
+        //if positive, add to end of bucket.
+        if (element > 0) digits[digit].add(element);
+        //if negative, add to beginning of bucket.
+        else digits[digit].add(element,0);
       }
+
+      /* PRINTING BUCKETS
       for (int i=0;i<10;i++) {
         System.out.println(i+": "+digits[i]);
-      }
+      }*/
+
 
       //now add elements back to data in new order
       MyLinkedList  out = new MyLinkedList();
@@ -32,7 +35,7 @@ public class Radix {
       //System.out.println(digits[0]);
 
       if (passes > 1) data = radix(out,1,passes);
-    }
+
   }
 
 
@@ -48,6 +51,7 @@ public class Radix {
       }
       return out;
      }
+
     //create array of linkedlists. (buckets)
     MyLinkedList[] digits = new MyLinkedList[10];
     //initialize eachbucket.
@@ -71,13 +75,28 @@ public class Radix {
     }
 
 
-    //now add elements back to data in new order
-    MyLinkedList out = new MyLinkedList();
-    for (int idx=0;idx<10;idx++) {
-      System.out.println(digits[idx]);
-      if (! digits[idx].equals(null)) out.extend(digits[idx]);
-    }
-    return radix(out,i+1,passes);
+
+      //now add elements back to data in new order
+      MyLinkedList out = new MyLinkedList();
+      int sIdx = digits[0].size();//start index for copying back on last call for negative numbers.
+      //link the lists together
+      for (int idx=0;idx<10;idx++) {
+        System.out.println(digits[idx]);
+        if (! digits[idx].equals(null)) out.extend(digits[idx]);
+      }
+
+      //if this is the last pass, start at sIdx and move any negatives to front.
+      if (i+ 1==passes) {
+        int cIdx = sIdx; // current index, just to not change sIdx.
+        for (int t=0;t<(out.size()-sIdx);t++) { // t counts how many elements you should count
+          if (out.get(cIdx) > 0) cIdx ++;
+          else out.add(out.remove(cIdx),0);
+        }
+      }
+
+      //regardless, call recursion
+      return radix(out,i+1,passes);
+
   }
 
   private static int maxDigits(int[] data) {
